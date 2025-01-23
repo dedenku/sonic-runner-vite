032f1ab8fc574a1dada8c9c183be48cf393fd6ae
dedenku <umamdkabal@gmail.com>
2025-01-23 23:20:03 +0700

    initial commit

---
+++ src/kaplayCtx.js
@@ -0,0 +1,22 @@
+// import kaplay
+import kaplay from "kaplay";
+// start kaplay
+
+const k = kaplay({
+    width: 1920,
+    height: 1080,
+    letterbox: true,
+    background: [0, 0, 0],
+    global: false,
+    buttons: {
+        jump: {
+            keyboard: ["space"],
+            mouse: "left",
+        },
+    },
+    touchToMouse: true,
+    debugKey: "d",
+    debug: true,
+});
+
+export default k;
---
+++ src/scenes/game.js
@@ -0,0 +1,77 @@
+import { makeMotobug } from "../entities/motobug";
+import makeSonic from "../entities/sonic";
+import k from "../kaplayCtx";
+
+export default function game() {
+    k.setGravity(3100);
+
+    const bgPieceWidth = 1920;
+    const bgPieces = [
+        k.add([k.sprite("chemical-bg"), k.pos(0, 0), k.scale(2), k.opacity(0.8)]),
+        k.add([k.sprite("chemical-bg"), k.pos(bgPieceWidth * 2, 0), k.scale(2), k.opacity(0.8)]),
+    ];
+
+    const platformWidth = 1280;
+    const platforms = [
+        k.add([k.sprite("platforms"), k.pos(0, 450), k.scale(4)]),
+        k.add([k.sprite("platforms"), k.pos(platformWidth * 4, 450), k.scale(4)]),
+    ];
+
+    const sonic = makeSonic(k.vec2(200, 745));
+    sonic.setControls();
+    sonic.setEvents();
+
+    let gameSpeed = 300;
+
+    k.loop(1, () => {
+        gameSpeed += 50;
+    });
+
+    const spawnMotoBug = () => {
+        const motoBug = makeMotobug(k.vec2(1950, 773));
+        motoBug.onUpdate(() => {
+            if (gameSpeed < 3000) {
+                motoBug.move(-(gameSpeed + 300), 0);
+                return;
+            }
+
+            motoBug.move(-gameSpeed, 0);
+        });
+
+        motoBug.onExitScreen(() => {
+            if (motoBug.pos.x < 0) k.destroy(motoBug);
+        });
+        const waitTime = k.rand(0.5, 2.5);
+        k.wait(waitTime, spawnMotoBug);
+    }
+
+    spawnMotoBug();
+
+    k.add([
+        k.rect(1920, 300),
+        k.pos(0, 832),
+        k.opacity(0),
+        k.area(),
+        k.body({ isStatic: true }),
+
+    ])
+
+    k.onUpdate(() => {
+        if (bgPieces[1].pos.x < 0) {
+            bgPieces[0].moveTo(bgPieces[1].pos.x + bgPieceWidth * 2, 0);
+            bgPieces.push(bgPieces.shift());
+        }
+
+        bgPieces[0].move(-100, 0);
+        bgPieces[1].moveTo(bgPieces[0].pos.x + bgPieceWidth * 2, 0);
+
+        if (platforms[1].pos.x < 0) {
+            platforms[0].moveTo(platforms[1].pos.x + platforms[1].width * 4, 450);
+            platforms.push(platforms.shift());
+        }
+
+        platforms[0].move(-gameSpeed, 0);
+        platforms[1].moveTo(platforms[0].pos.x + platforms[1].width * 4, 450);
+    })
+
+}
